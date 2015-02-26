2c216c5cd354ba5fe745d1b1c27c0bbfc41bec75
Zlatan Zaric <zlatan.zaric@student.tugraz.at>
2015-02-26 15:07:40 +0100

    OBDTool main added

---
+++ OBDTool/src/Main.cpp
@@ -0,0 +1,21 @@
+/*
+ * Main.cpp
+ *
+ *  Created on: Feb 26, 2015
+ *      Author: zlatan
+ */
+
+#include "Main.h"
+
+namespace main {
+
+Main::Main() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Main::~Main() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace main */
