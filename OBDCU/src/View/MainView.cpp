3eb3a80caad95cbc4f156746f90ddc31d5c929b8
Zlatan Zaric <zlatan.zaric@student.tugraz.at>
2015-02-26 17:11:11 +0100

    Cmake Lists setup

---
+++ OBDCU/src/View/MainView.cpp
@@ -0,0 +1,21 @@
+/*
+ * MainView.cpp
+ *
+ *  Created on: Feb 26, 2015
+ *      Author: zlatan
+ */
+
+#include "MainView.h"
+
+namespace main {
+
+MainView::MainView() {
+	// TODO Auto-generated constructor stub
+
+}
+
+MainView::~MainView() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace main */
---
+++ OBDCU/src/View/includes/MainView.h
@@ -0,0 +1,20 @@
+/*
+ * MainView.h
+ *
+ *  Created on: Feb 26, 2015
+ *      Author: zlatan
+ */
+
+#ifndef MAINVIEW_H_
+#define MAINVIEW_H_
+
+namespace main {
+
+class MainView {
+public:
+	MainView();
+	virtual ~MainView();
+};
+
+} /* namespace main */
+#endif /* MAINVIEW_H_ */
