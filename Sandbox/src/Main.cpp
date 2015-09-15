9a739c5a1181f09036605b5a0d233f94279baea4
Zlatan Zaric <zlatan.zaric@student.tugraz.at>
2015-09-15 16:45:43 +0200

    Sandbox

---
+++ Sandbox/src/Main.cpp
@@ -0,0 +1,84 @@
+/*
+ * Main.cpp
+ *
+ *  Created on: Sep 8, 2015
+ *      Author: zlatan
+ */
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <iostream>
+#include <fstream>
+#include <string.h>
+#include <termios.h>
+#include <errno.h>
+
+int main(int argc, char* argv[]) {
+
+	if (argc < 3) {
+		std::cout << "Wrong argument count!" << std::endl;
+		return -1;
+	}
+
+	char* devicename = argv[1];
+	int file = open(devicename, O_RDWR | O_NONBLOCK | O_NOCTTY);
+	int flags = fcntl(file, F_GETFL, 0);
+
+	if (fcntl(file, F_SETFL, flags | O_NONBLOCK)) {
+		std::cout << "read non blocking" << std::endl;
+	}
+
+	if (file < 0) {
+		std::cout << "Error in creating fd.\n";
+	}
+	struct termios oldattr;
+	tcgetattr(file, &oldattr);
+	struct termios attr;
+
+	attr.c_cflag = B38400 | CRTSCTS | CS8 | CLOCAL | CREAD;
+	attr.c_iflag = IGNPAR;
+	attr.c_oflag = 0;
+	attr.c_lflag = 0; //ICANON;
+	attr.c_cc[VMIN] = 1;
+	attr.c_cc[VTIME] = 0;
+//	cfsetospeed(&attr, B38400);
+//	cfsetispeed(&attr, B38400);
+
+	if (tcflush(file, TCIFLUSH) < 0 || tcflush(file, TCOFLUSH) < 0
+			|| tcsetattr(file, TCSANOW, &attr) < 0) {
+		std::cout << "error at setting baud" << std::endl;
+		return -1;
+	}
+
+	while (true) {
+		std::cout << "sending at i" << std::endl;
+		if (write(file, "at i", 4) < 0) {
+			std::cout << "error while writing" << std::endl;
+		}
+		std::cout << "receiving response" << std::endl;
+		char buff[256];
+		int count = 0;
+
+//	if((count=read(file, buff, 256)) < 0 ){
+//		std::cout<< "error " << errno << " " << count << std::endl;
+//	}
+
+		memset(buff, '\0', 256);
+
+		while ((count = read(file, buff, 256)) < 0 && errno == EAGAIN) {
+			std::cout << "sleeping" << std::endl;
+			sleep(3);
+		}
+
+		do {
+			std::cout << "count: " << count << " buffer : " << std::endl;
+			for (int i = 0; i < 256; i++) {
+				printf("%x ", buff[i]);
+			}
+			std::cout << std::endl;
+		} while ((count = read(file, buff, 256)) >= 0);
+	}
+	close(file);
+}
+
