6d2fe03b4984da70bca540c3eb33537ce89a8a8a
Zlatan Zaric <zlatan.zaric@student.tugraz.at>
2015-09-27 15:04:03 +0200

    installing script for linux packages added

--- libs/packages_to_install.sh
+++ libs/packages_to_install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-sudo apt-get install libcppunit-dev libcppunit-1.13-0 libcppunit-doc libboost-all-dev libusb-dev
+sudo apt-get install libcppunit-dev libcppunit-1.13-0 libcppunit-doc libboost-all-dev libusb-dev libavcodec-dev libavformat-dev libavutil-dev libdlna0 libdc1394-22-dev libgsm1-dev libogg-dev libraw1394-dev libschroedinger-dev libspeex-dev libtheora-dev libvorbis-dev ushare libavcodec-dev libavformat-dev libavutil-dev libdlna-dev libdlna0
+
