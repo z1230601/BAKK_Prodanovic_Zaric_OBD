d137cde3552399035d1f37ec34b4c0bb4d75eb07
Zlatan Zaric <zlatan.zaric@student.tugraz.at>
2015-11-08 16:53:55 +0100

    added cmake module for libxml++ and added sample configuration for database xml

--- ubuntuAptGetInstalls.sh
+++ ubuntuAptGetInstalls.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo apt-get install libmysqlcppconn-dev mysql-server libusb-vhci libusb
+sudo apt-get install libmysqlcppconn-dev mysql-server libusb-vhci libusb libxml++2.6-2 libxml++2.6-2-dev libxml++2.6-2-doc
