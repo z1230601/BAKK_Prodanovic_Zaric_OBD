cd1e2e3a523da3591974ee47806cfe7f3db46483
Zlatan Zaric <zlatan.zaric@student.tugraz.at>
2015-12-25 17:03:14 +0100

    refactored to recursive tag reading

--- OBDMiddleware/configurations/src/XMLReader.cpp
+++ OBDMiddleware/configurations/src/XMLReader.cpp
@@ -9,32 +9,43 @@
 #include <libxml++/libxml++.h>
 #include <iostream>
 
-XMLReader::XMLReader(DefaultXMLHandler* handler):
-handler_(handler){}
+XMLReader::XMLReader(DefaultXMLHandler* handler)
+        : handler_(handler)
+{
+}
 
-XMLReader::~XMLReader(){}
+XMLReader::~XMLReader()
+{
+}
 
-
-bool XMLReader::parseFile(std::string configurationFile) {
-    try{
-    xmlpp::DomParser parser;
-    parser.set_validate();
-    parser.set_substitute_entities();
-    parser.parse_file(configurationFile);
-    if (parser) {
-        const xmlpp::Node* currentNode = parser.get_document()->get_root_node();
-
-        xmlpp::Node::NodeList children = currentNode->get_children();
-        for(xmlpp::Node::NodeList::iterator it = children.begin(); it != children.end(); ++it) {
-            handler_->handleNode(*it);
+bool XMLReader::parseFile(std::string configurationFile)
+{
+    try
+    {
+        xmlpp::DomParser parser;
+        parser.set_validate();
+        parser.set_substitute_entities();
+        parser.parse_file(configurationFile);
+        if(parser)
+        {
+            iterateChildren(parser.get_document()->get_root_node());
         }
-    }
-    }catch(std::exception & e){
+    } catch (std::exception & e)
+    {
         std::cout << e.what() << std::endl;
         return false;
     }
     return true;
 }
 
-
+void XMLReader::iterateChildren(const xmlpp::Node* parent)
+{
+    xmlpp::Node::NodeList children = parent->get_children();
+    for(xmlpp::Node::NodeList::iterator it = children.begin();
+            it != children.end(); ++it)
+    {
+        handler_->handleNode(*it);
+        iterateChildren(*it);
+    }
+}
 
