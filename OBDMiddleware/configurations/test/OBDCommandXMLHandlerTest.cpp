acddf9aed6d34d05796f8bb131dc37bb59012d06
Zlatan Zaric <zlatan.zaric@student.tugraz.at>
2015-12-27 20:59:05 +0100

    commented test added again

--- OBDMiddleware/configurations/test/OBDCommandXMLHandlerTest.cpp
+++ OBDMiddleware/configurations/test/OBDCommandXMLHandlerTest.cpp
@@ -11,49 +11,44 @@ void OBDCommandXMLHandlerTest::tearDown()
 
 void OBDCommandXMLHandlerTest::testServiceModeParse()
 {
-    try{
-        std::cout << doc_->write_to_string() << std::endl;
-    }catch(std::exception &e){
-        std::cout << "Caught exception: " << e.what() << std::endl;
+
+    xmlpp::Node* first_service_mode_tag;
+
+    for(xmlpp::Node::NodeList::iterator it = doc_
+            ->get_root_node()->get_children().begin();
+            it != doc_->get_root_node()->get_children().end();
+            ++it)
+    {
+        if((*it)->get_name().compare("servicemode") == 0){
+            first_service_mode_tag = (*it);
+            break;
+        }
     }
 
-//    xmlpp::Node* first_service_mode_tag;
-//
-//    for(xmlpp::Node::NodeList::iterator it = parser_.get_document()
-//            ->get_root_node()->get_children().begin();
-//            it != parser_.get_document()->get_root_node()->get_children().end();
-//            ++it)
-//    {
-//        if((*it)->get_name().compare("servicemode") == 0){
-////            first_service_mode_tag = (*it);
-//            break;
-//        }
-//    }
-//
-//    std::cout << "First service mode tag is:" << std::endl;
-//    std::cout << first_service_mode_tag->get_name() << std::endl;
-//    xmlpp::Node::NodeList
-//    root_children = first_service_mode_tag->get_children();
-//
-//    std::cout << first_service_mode_tag->get_name() << std::endl;
-//    handler_for_test_->handleNode(first_service_mode_tag);
-//
-//    for(xmlpp::Node::NodeList::iterator it = root_children.begin();
-//            it != root_children.end(); ++it)
-//    {
-//        std::cout << (*it)->get_name() << std::endl;
-//        if((*it)->get_name().compare("obdcommands") == 0)
-//        {
-//            break;
-//        }
-//
-//        handler_for_test_->handleNode(*it);
-//
-//    }
-//
-//    CPPUNIT_ASSERT_EQUAL((size_t) 1, handler_for_test_->getParsedData().size());
-//    CPPUNIT_ASSERT_EQUAL((size_t) 2,
-//            handler_for_test_->getParsedData().at(0).sid_.size());
+    std::cout << "First service mode tag is:" << std::endl;
+    std::cout << first_service_mode_tag->get_name() << std::endl;
+    xmlpp::Node::NodeList
+    root_children = first_service_mode_tag->get_children();
+
+    std::cout << first_service_mode_tag->get_name() << std::endl;
+    handler_for_test_->handleNode(first_service_mode_tag);
+
+    for(xmlpp::Node::NodeList::iterator it = root_children.begin();
+            it != root_children.end(); ++it)
+    {
+        std::cout << (*it)->get_name() << std::endl;
+        if((*it)->get_name().compare("obdcommands") == 0)
+        {
+            break;
+        }
+
+        handler_for_test_->handleNode(*it);
+
+    }
+
+    CPPUNIT_ASSERT_EQUAL((size_t) 1, handler_for_test_->getParsedData().size());
+    CPPUNIT_ASSERT_EQUAL((size_t) 2,
+            handler_for_test_->getParsedData().at(0).sid_.size());
 }
 
 ///////////////////////////////////////////CONSTRUCTION SECTION /////////////////////////////////////////////////////////////
